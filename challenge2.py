5d25f5e4ff4105242a98863cc9cc68e92d2cac23
Tuomas Salmi <salmi.tuomas@gmail.com>
2015-10-27 00:13:38 +0200

    A minor change to challenge2

--- challenge2.py
+++ challenge2.py
@@ -2,8 +2,8 @@ from challenge1 import hexTobase64
 import binascii
 
 def xorhex(hex1, hex2):
-   return hex(hex1 ^ hex2)
+   return hex(int(hex1, 16) ^ int(hex2, 16))
 
 
 if __name__ == "__main__":
-    print(xorhex(0x1c0111001f010100061a024b53535009181c, 0x686974207468652062756c6c277320657965))
+    print(xorhex("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965"))
